b96467c509fec6be5fe2425ec5e5d6fd57018762
Tomas Vukojicic <tomasvukojicic@gmail.com>
2026-04-16 07:53:47 -0300

    chore: bump version to v0.0.25-LOGO

--- src/components/SimulationToggle.tsx
+++ src/components/SimulationToggle.tsx
@@ -72,7 +72,7 @@ export default function SimulationToggle() {
         </div>
       </div>
       <div style={{ paddingLeft: '15px', fontSize: '10px', fontWeight: '900', color: '#888', letterSpacing: '0.05em' }}>
-        v0.0.24-ONBOARDING
+        v0.0.25-LOGO
       </div>
     </div>
   );
