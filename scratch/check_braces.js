54971a63ec93d7bf006c351e56435d16156dc1c9
Tomas Vukojicic <tomasvukojicic@gmail.com>
2026-04-19 16:07:07 -0300

    chore: cleanup scratch files

--- scratch/check_braces.js
+++
@@ -1,9 +0,0 @@
-
-const fs = require('fs');
-const content = fs.readFileSync('/Users/tomasvukojicic/Alimnet v3/src/app/admin/page.tsx', 'utf8');
-let open = 0;
-for (let i = 0; i < content.length; i++) {
-  if (content[i] === '{') open++;
-  if (content[i] === '}') open--;
-}
-console.log('Balance:', open);
