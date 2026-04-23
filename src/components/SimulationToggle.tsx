d353a4e72ebc4bfffa438db60c0c45d6615f74f6
Tomas Vukojicic <tomasvukojicic@gmail.com>
2026-04-23 16:42:59 -0300

    chore: bump version to v1.6.46

--- src/components/SimulationToggle.tsx
+++ src/components/SimulationToggle.tsx
@@ -72,7 +72,7 @@ export default function SimulationToggle() {
         </div>
       )}
       <div style={{ paddingLeft: '15px', fontSize: '10px', fontWeight: '900', color: '#888', letterSpacing: '0.05em', opacity: 0.6 }}>
-        v1.6.45
+        v1.6.46
       </div>
     </div>
   );
